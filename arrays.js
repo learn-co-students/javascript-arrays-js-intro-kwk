d5dbbe10d0d0e46f42a642cbd49dd436956e1a1f
Adriana Zestroiu <adriana.zestroiu@yahoo.com>
2016-08-22 21:55:17 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,47 @@
+var chocolateBars = [ "snickers", "hundred grand", "kit kat", "skittles"];
+
+function addElementToBeginningOfArray(array,element){
+var array=[1];
+return (["foo",...array]);
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+var array=[1];
+array=array.unshift('foo');
+return array;
+}
+
+function addElementToEndOfArray(array,element){
+var array=[1];
+return ([...array,"foo"]);
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+var array=[1];
+array=array.push('foo');
+return array;
+}
+
+function accessElementInArray(array,index){
+return (array[index]);
+}
+function destructivelyRemoveElementFromBeginningOfArray(array)
+{
+  var array=['foo',1];
+  array.shift('foo');
+  return array;
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  var array=[1,'foo'];
+  array.push('foo');
+  return array;
+}
+function removeElementFromBeginningOfArray(array){
+var array=[1,2,3];
+return array.slice(1);
+}
+
+function removeElementFromEndOfArray(array){
+var array=[1,2,3];
+return array.slice(0,array.length-1);
+}
