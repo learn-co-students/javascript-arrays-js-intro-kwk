c9d2b4bbd059850194ae7656dec6e4e4e95dfe75
Kimia Kalantari <kimiakalantari1356@gmail.com>
2016-12-16 23:40:29 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -2,14 +2,14 @@ var chocolateBars = ['snickers', 'hundred grands', 'kit kat', 'skittles'];
 
 function addElementToBeginningOfArray (array, element){
   var array = [1]; 
-  array.unshift("foo")
+  array.unshift('foo')
   return array 
 }
 addElementToBeginningOfArray(); 
 
 function destructivelyAddElementToBeginningOfArray (array, element){
-  array = [1, 5];
-  array.unshift("foo")
+  array = ['foo', 1];
+  array.unshift('foo')
   return array 
 }
 destructivelyAddElementToBeginningOfArray(); 
