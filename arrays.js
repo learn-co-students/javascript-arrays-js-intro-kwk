644c8fc66935f7002ef7b886586138eacc0b4ed7
Savannah Nester <nester_savannah@yahoo.com>
2017-07-25 16:17:43 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var addElementToBeginningOfArray = ["array"]
+["element", ...addElementToBeginningOfArray]
+var destructivelyAddElementToBeginningOfArray = ["array"]
+destructivelyAddElementToBeginningOfArray.unshift("element")
+var addElementToEndOfArray = ["array"]
+[...addElementToEndOfArray, "element"]
+var destructivelyAddElementToEndOfArray = ["array"]
+destructivelyAddElementToEndOfArray.push("element")
